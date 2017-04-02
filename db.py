856072d0f22064d4add941a8b713085ba8a41d6e
Stefan Pahlplatz <stefan314@live.nl>
2017-04-02 14:14:22 +0200

    Table now gets automatically created.

--- db.py
+++ db.py
@@ -11,6 +11,21 @@ class Database:
         self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
         self.cursor = self.connection.cursor()
 
+        stmt = "SHOW TABLES LIKE 'products'"
+        self.cursor.execute(stmt)
+        result = self.cursor.fetchone()
+        if not result:
+            # there are no tables named "tableName"
+            sql = """CREATE TABLE `products` (
+                              `name` varchar(100) NOT NULL,
+                              `price` double NOT NULL,
+                              `brand` varchar(45) NOT NULL,
+                              `weight` varchar(45) NOT NULL,
+                              `group` varchar(45) NOT NULL,
+                              PRIMARY KEY (`name`,`price`,`brand`,`weight`)
+                          ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"""
+            self.cursor.execute(sql)
+
     def insert(self, name, price, brand, weight, group):
         try:
             query = "INSERT INTO products (name, price, brand, weight, `group`) VALUES ('" + \
